473ff0241710d141706e3e0b74acbc382e1e8c02
Andreas Berzelius <andreas.berzelius@tietoevry.com>
2023-04-05 15:52:52 +0200

    Now init commit

---
+++ emsdk/hello.c
@@ -0,0 +1,13 @@
+// hello.c
+#include <stdio.h>
+#include <emscripten.h>
+
+EM_JS(void, call_alert, (), {
+  alert('hello world!');
+  throw 'all done';
+});
+
+int main() {
+  call_alert();
+  return 0;
+}
